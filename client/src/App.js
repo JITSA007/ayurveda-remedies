829c959c61f25239aa1755509e2042225ef50579
Jitendra Prajapat <jps007@ymail.com>
2025-02-11 23:22:31 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,24 @@
+// client/src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Recipes from './pages/Recipes';
+import Ingredients from './pages/Ingredients';
+import DoshaAnalysis from './pages/DoshaAnalysis';
 
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/ingredients" element={<Ingredients />} />
+          <Route path="/dosha-analysis" element={<DoshaAnalysis />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
