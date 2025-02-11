771c53d6bfbe44ca426a08a2fe68744ddd0364a3
Jitendra Prajapat <jps007@ymail.com>
2025-02-11 23:24:07 +0530

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,1 +1,19 @@
 
+// client/src/components/Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/recipes">Recipes</Link>
+        <Link to="/ingredients">Ingredients</Link>
+        <Link to="/dosha-analysis">Dosha Analysis</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
