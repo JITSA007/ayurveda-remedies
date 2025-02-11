7e35a710159d2506fb5ad724ea869765a834773f
Jitendra Prajapat <jps007@ymail.com>
2025-02-11 23:23:03 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,1 +1,13 @@
+// client/src/pages/Home.js
+import React from 'react';
 
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Ayurveda Remedies</h1>
+      <p>Discover natural remedies using household ingredients.</p>
+    </div>
+  );
+}
+
+export default Home;
