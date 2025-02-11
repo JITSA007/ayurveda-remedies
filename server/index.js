a7b581eeed207b153b0f583d5cb416658f88f557
Jitendra Prajapat <jps007@ymail.com>
2025-02-11 23:10:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,34 @@
+// server/index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
+// Load environment variables
+dotenv.config();
+
+// Initialize app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes (we'll add these later)
+app.get('/', (req, res) => {
+  res.send('Welcome to the Ayurveda Remedies API!');
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
